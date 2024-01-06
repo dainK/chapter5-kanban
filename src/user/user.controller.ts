3072e62cbfea6c17387ed5c3c67c9e1ea5fc32fc
ay0530 <edkdud530@gmail.com>
2024-01-07 01:43:10 +0900

    ✨ feat: 로그아웃 API 추가, 회원 탈퇴 시 리프레시 토큰 삭제 기능 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RedisService } from 'src/redis/redis.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginDto } from './dto/login.dto';
@@ -8,7 +9,10 @@ import { JwtAuthGuard } from 'src/auth/jwt.guard';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly redisService: RedisService,
+  ) {}
 
   @Post('signup')
   async signup(@Body() createDto: CreateUserDto) {
@@ -19,7 +23,15 @@ export class UserController {
   async login(@Body() loginDto: LoginDto) {
     return await this.userService.login(loginDto.email, loginDto.password);
   }
-  
+
+  @Post('logout')
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
+  async logout(@Req() req) {
+    // 프론트에서 로컬스토리지에 저장한 액세스 토큰을 지워줘야 할 것으로 생각 - 이아영
+    await this.redisService.removeRefreshToken(req.user.id); // 리프레시 토큰 삭제
+    return { message: '로그아웃이 되었습니다' };
+  }
+
   @Get('profile')
   @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   async getProfile(@Req() req) {
@@ -42,7 +54,10 @@ export class UserController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  async remove(@Param('id') id: string) {
+    await this.userService.remove(+id);
+    // 프론트에서 로컬스토리지에 저장한 액세스 토큰을 지워줘야 할 것으로 생각 - 이아영
+    await this.redisService.removeRefreshToken(id); // 리프레시 토큰 삭제
+    return { message: '회원 탈퇴가 완료되었습니다' };
   }
 }
