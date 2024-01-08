41aa3ee73c680d9c64ab6b8cc579f737db72a9cd
ay0530 <edkdud530@gmail.com>
2024-01-08 23:53:24 +0900

    ♻️ refactor: board-column CRUD 리팩토링

--- src/board-column/board-column.service.ts
+++ src/board-column/board-column.service.ts
@@ -20,23 +20,9 @@ export class BoardColumnService {
     private readonly boardMemberRepository: Repository<BoardMember>,
   ) {}
   async create(createBoardColumnDto: CreateBoardColumnDto, board_id: number, user_id: number) {
-    // 보드 조회
-    const existingBoard = await this.boardRepository.findOne({ where: { id: board_id } });
-
-    // ERR : 보드가 존재하지 않을 경우
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
-    }
-
-    // 제목 조회
-    const existingColumnTitle = await this.boardColumnRepository.findOne({
-      where: { title: createBoardColumnDto.title },
-    });
-
-    // ERR : 이미 제목이 존재할 경우
-    if (existingColumnTitle) {
-      throw new ConflictException('이미 칼럼 제목이 존재합니다.');
-    }
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMember(board_id, user_id); // 보드 멤버 조회
+    await this.findOneByTitle(createBoardColumnDto.title); // 제목 중복 여부 체크
 
     // 칼럼 정보 저장
     const boardColum = await this.boardColumnRepository.save({
@@ -47,30 +33,15 @@ export class BoardColumnService {
     return { message: '칼럼 저장이 완료되었습니다.', boardColum };
   }
 
-  // 특정 보드의 칼럼 전체 조회(칼럼 멤버들만~)
+  // 특정 보드의 칼럼 전체 조회(보드 멤버들만~)
   async findAll(board_id: number, user_id: number) {
-    // 보드 조회
-    const existingBoard = await this.boardRepository.findOne({
-      where: { id: board_id },
-    });
-
-    // ERR : 보드가 존재하지 않을 경우
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
-    }
-
-    // 로그인 한 사용자가 소유된 보드들 조회
-    const exsitingBoard = await this.boardMemberRepository.findOne({ where: { board_id, user_id } });
-
-    // ERR : 포함된 보드가 존재가 하지 않을 경우
-    if (!exsitingBoard) {
-      throw new NotFoundException('권한이 없습니다.');
-    }
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMember(board_id, user_id); // 보드 멤버 조회
 
     // 칼럼 목록 조회
     const columns = await this.boardColumnRepository.find({ where: { board_id } });
 
-    // ERR : 포함된 보드가 존재가 하지 않을 경우
+    // ERR : 칼럼이 없는 경우
     if (!columns) {
       throw new NotFoundException('칼럼이 존재하지 않습니다.');
     }
@@ -78,104 +49,91 @@ export class BoardColumnService {
     return { columns };
   }
 
-  // 하나씩은 조회할 필요가.. 없던가..?
+  // 칼럼 상세 조회
   async findOne(id: number, board_id: number, user_id: number) {
-    // 보드 조회
-    const existingBoard = await this.boardRepository.findOne({
-      where: { id: board_id },
-    });
-
-    // ERR : 보드가 존재하지 않을 경우
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
-    }
-
-    // 로그인 한 사용자가 소유된 보드들 조회
-    const exsitingBoard = await this.boardMemberRepository.findOne({ where: { board_id, user_id } });
-
-    // ERR : 포함된 보드가 존재가 하지 않을 경우
-    if (!exsitingBoard) {
-      throw new NotFoundException('권한이 없습니다.');
-    }
-
-    // 칼럼 상세 조회
-    const column = await this.boardColumnRepository.findOne({ where: { id } });
-    console.log('column: ', column);
-
-    // ERR : 포함된 칼럼이 존재가 하지 않을 경우
-    if (_.isNil(column)) {
-      throw new NotFoundException('칼럼이 존재하지 않습니다.');
-    }
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMember(board_id, user_id); // 보드 멤버 조회
+    const column = await this.findOneColumn(id); // 칼럼 상세 조회
 
     return { column };
   }
 
   async update(id: number, updateBoardColumnDto: UpdateBoardColumnDto, board_id: number, user_id: number) {
-    // 보드 조회
-    const existingBoard = await this.boardRepository.findOne({
-      where: { id: board_id },
-    });
-
-    // ERR : 보드가 존재하지 않을 경우
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
-    }
-
-    // 로그인 한 사용자가 소유된 보드들 조회
-    const exsitingBoard = await this.boardMemberRepository.findOne({ where: { board_id, user_id } });
-
-    // ERR : 포함된 보드가 존재가 하지 않을 경우
-    if (!exsitingBoard) {
-      throw new NotFoundException('권한이 없습니다.');
-    }
-
-    // 조회하려는 칼럼에 로그인 한 사용자가 멤버로 추가되어 있는지 검사
-    // role이 0(Admin) 또는 1(Editor) 일 때 가능
-    // join문으로 처리해야하남? - 이아영
-    const checkBoardMemberRole = await this.boardMemberRepository.findOne({
-      where: [
-        { id, user_id, role: 0 },
-        { id, user_id, role: 1 },
-      ],
-    });
-
-    // ERR : viewer일 경우
-    if (!checkBoardMemberRole) {
-      throw new UnauthorizedException('권한이 존재하지 않습니다.');
-    }
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMember(board_id, user_id); // 보드 멤버 조회
+    await this.findOneColumn(id); // 칼럼 상세 조회
+    await this.checkBoardMemberRole(board_id, user_id); // 로그인 한 사용자의 role 조회
+    await this.findOneByTitle(updateBoardColumnDto.title); // 제목 중복 여부 체크
 
     // 칼럼 업데이트
-    const column = await this.boardColumnRepository.update({ id }, { title: updateBoardColumnDto.title });
+    await this.boardColumnRepository.update({ id }, { title: updateBoardColumnDto.title });
     return { column: { title: updateBoardColumnDto.title } };
   }
 
   async remove(id: number, board_id: number, user_id: number) {
-    // 칼럼 상세 조회
-    const existingBoard = await this.boardRepository.findOne({
-      where: { id: board_id },
-    });
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.findOneColumn(id); // 칼럼 상세 조회
+    await this.checkBoardMemberRole(board_id, user_id); // 로그인 한 사용자의 role 조회
 
-    // ERR : 칼럼가 존재하지 않을 경우
+    // 칼럼 삭제
+    await this.boardColumnRepository.delete({ id });
+    return { message: '칼럼 삭제가 완료되었습니다.' };
+  }
+
+  // 보드 존재 여부 확인
+  async findOneBoard(id: number): Promise<Board | any> {
+    const existingBoard = await this.boardRepository.findOne({ where: { id } });
+    // ERR : 보드가 존재하지 않을 경우
     if (!existingBoard) {
-      throw new NotFoundException('칼럼가 존재하지 않습니다.');
+      throw new NotFoundException('보드가 존재하지 않습니다.');
+    }
+    return existingBoard;
+  }
+
+  // 제목 존재 여부 확인
+  async findOneByTitle(title: string): Promise<Board | any> {
+    const existingTitle = await this.boardColumnRepository.findOne({ where: { title } });
+    // ERR : 이미 제목이 존재할 경우
+    if (existingTitle) {
+      throw new ConflictException('이미 보드 제목이 존재합니다.');
+    }
+  }
+
+  // 보드 멤버 여부 조회
+  async checkBoardMember(board_id: number, user_id: number): Promise<BoardMember | any> {
+    const exsitingBoardMember = await this.boardMemberRepository.findOne({ where: { board_id, user_id } });
+
+    // ERR : 보드 멤버가 아닌 경우
+    if (!exsitingBoardMember) {
+      throw new NotFoundException('권한이 없습니다.');
+    }
+  }
+
+  // 칼럼 존재 여부 확인
+  async findOneColumn(id: number): Promise<BoardColumn | any> {
+    const existingColumn = await this.boardColumnRepository.findOne({ where: { id } });
+    // ERR : 칼럼이 존재하지 않을 경우
+    if (!existingColumn) {
+      throw new NotFoundException('칼럼이 존재하지 않습니다.');
     }
 
-    // 조회하려는 칼럼에 로그인 한 사용자가 멤버로 추가되어 있는지 검사
-    // role이 0(Admin) 또는 1(Editor) 일 때 가능
+    return existingColumn;
+  }
+
+  // 로그인 한 사용자의 권한 확인
+  async checkBoardMemberRole(board_id: number, user_id: number): Promise<Board | any> {
+    // role이 0(Admin) 또는 1(Editor) 일 때 수정/삭제 가능
     // join문으로 처리해야하남? - 이아영
-    const checkBoardMemberRole = await this.boardMemberRepository.findOne({
+    const existingBoardMember = await this.boardMemberRepository.findOne({
       where: [
-        { id, user_id, role: 0 },
-        { id, user_id, role: 1 },
+        { board_id, user_id, role: 0 },
+        { board_id, user_id, role: 1 },
       ],
     });
 
-    // ERR : viewer일 경우
-    if (!checkBoardMemberRole) {
+    // ERR : role가 0(Admin), 1(Editor)이 아닌 경우
+    if (!existingBoardMember) {
       throw new UnauthorizedException('권한이 존재하지 않습니다.');
     }
-
-    const column = await this.boardColumnRepository.delete({ id });
-    return { message: '칼럼 삭제가 완료되었습니다.' };
   }
 }
