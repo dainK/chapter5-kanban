7c0d8ff9583e4d8c78f4143d496f780a30f93805
ay0530 <edkdud530@gmail.com>
2024-01-09 00:21:20 +0900

    ♻️ refactor: board-member CRUD 리팩토링

--- src/board-member/board-member.controller.ts
+++ src/board-member/board-member.controller.ts
@@ -15,22 +15,19 @@ export class BoardMemberController {
 
   @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   @Get(':board_id')
-  findAll(@Param('board_id') board_id: number) {
-    return this.boardMemberService.findAll(board_id);
+  findAll(@Param('board_id') board_id: number, @Req() req) {
+    return this.boardMemberService.findAll(board_id, req.user.id);
   }
 
   @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
-  @Get(':board_id/:id')
-  findOne(@Param('id') id: string, @Param('board_id') board_id: number) {
-    return this.boardMemberService.findOne(+id, board_id);
+  @Get(':board_id/:user_id')
+  findOne(@Param('user_id') select_user_id: number, @Param('board_id') board_id: number, @Req() req) {
+    return this.boardMemberService.findOne(board_id, req.user.id, select_user_id);
   }
 
   @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   @Patch(':board_id/:user_id')
   update(@Param('id') id: string, @Req() req, @Param('board_id') board_id: number, @Param('user_id') select_user_id: number, @Body('role') role: number) {
-    console.log('role: ', role);
-    console.log('select_user_id: ', select_user_id);
-    console.log('board_id: ', board_id);
     return this.boardMemberService.update(+id, req.user.id, board_id, select_user_id, role);
   }
 
--- src/board-member/board-member.service.ts
+++ src/board-member/board-member.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 import _ from 'lodash';
 import { Repository } from 'typeorm';
@@ -16,22 +16,10 @@ export class BoardMemberService {
   ) {}
 
   async create(board_id: number, user_id: number, select_user_id: number, role: number) {
-    // 보드 존재 여부 확인
-    const existingBoard = await this.findBoard(board_id);
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
-    }
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMemberRole(board_id, user_id); // 로그인 한 사용자의 role 조회
 
-    // 로그인 한 사용자의 권한 조회
-    const checkBoardMemberRole = await this.findBoardMember(board_id, user_id);
-
-    // ERR : role이 0(Admin) 또는 1(Editor)이 아닌 경우
-    if (!checkBoardMemberRole) {
-      throw new NotFoundException('권한이 없습니다.');
-    }
-
-    // 보드 멤버 조회
-    const existingBoardMember = await this.findBoardMember(board_id, select_user_id);
+    const existingBoardMember = await this.findBoardMember(board_id, select_user_id); // 보드 멤버 조회
     if (existingBoardMember) {
       throw new ConflictException('이미 등록된 멤버입니다.');
     }
@@ -46,80 +34,59 @@ export class BoardMemberService {
     return { message: '보드 멤버 추가가 완료되었습니다.' };
   }
 
-  async findAll(board_id: number) {
-    // 보드 존재 여부 확인
-    const existingBoard = await this.findBoard(board_id);
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
-    }
+  async findAll(board_id: number, user_id: number) {
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMember(board_id, user_id); // 보드 멤버 조회
 
     // 보드 멤버 목록 조회
     const boardMembers = await this.boardMemberRepository.find({ where: { board_id } });
-    if (_.isNil(boardMembers)) {
+    if (boardMembers.length === 0) {
       throw new NotFoundException('멤버가 존재하지 않습니다.');
     }
 
     return { boardMembers };
   }
 
-  async findOne(id: number, board_id: number) {
-    // 보드 존재 여부 확인
-    const existingBoard = await this.findBoard(board_id);
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
-    }
+  async findOne(board_id: number, user_id: number, select_user_id: number) {
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMember(board_id, user_id); // 보드 멤버 조회
 
-    // 회원정보 조회
-    // join문으로 이메일, 이름 가져오는 기능 추가하기!!!!!!
-    const boardMember = await this.boardMemberRepository.findOne({
-      where: { id, board_id },
-    });
-
-    // ERR : 포함된 보드가 존재하지 않을 경우
+    const boardMember = await this.findBoardMember(board_id, select_user_id); // 보드 멤버 조회
     if (!boardMember) {
-      throw new NotFoundException('멤버가 존재하지 않습니다.');
+      throw new ConflictException('멤버가 등록되지 않았습니다.');
     }
 
     return { boardMember };
   }
 
   async update(id: number, board_id: number, user_id: number, select_user_id: number, role: number) {
-    // 보드 존재 여부 확인
-    const existingBoard = await this.findBoard(board_id);
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMemberRole(board_id, user_id); // 로그인 한 사용자의 role 조회
+    
+    const boardMember = await this.findBoardMember(board_id, select_user_id); // 보드 멤버 조회
+    if (!boardMember) {
+      throw new ConflictException('멤버가 등록되지 않았습니다.');
     }
 
-    // 로그인 한 사용자의 권한 조회
-    const checkBoardMemberRole = await this.findBoardMember(board_id, user_id);
-
-    // ERR : role이 0(Admin) 또는 1(Editor)이 아닌 경우
-    if (!checkBoardMemberRole) {
-      throw new NotFoundException('권한이 없습니다.');
+    // ERR : 삭제하려는 멤버의 role이 0(Admin)인 경우
+    if (boardMember.role === 0) {
+      throw new NotFoundException('Admin은 수정할 수 없습니다.');
     }
-
     // 보드 멤버 업데이트
-    const board = await this.boardMemberRepository.update({ board_id, user_id: select_user_id }, { role });
+    await this.boardMemberRepository.update({ board_id, user_id: select_user_id }, { role });
     return { message: '권한 수정이 완료되었습니다.' };
   }
 
   async remove(id: number, board_id: number, user_id: number, select_user_id: number) {
-    // 보드 존재 여부 확인
-    const existingBoard = await this.findBoard(board_id);
-    if (!existingBoard) {
-      throw new ConflictException('보드가 존재하지 않습니다.');
+    await this.findOneBoard(board_id); // 보드 정보 조회
+    await this.checkBoardMemberRole(board_id, user_id); // 로그인 한 사용자의 role 조회
+
+    const boardMember = await this.findBoardMember(board_id, select_user_id); // 보드 멤버 조회
+    if (!boardMember) {
+      throw new ConflictException('멤버가 등록되지 않았습니다.');
     }
-
-    // 로그인 한 사용자의 권한 조회
-    const checkBoardMemberRole = await this.findBoardMember(board_id, user_id);
-
-    // ERR : role이 0(Admin) 또는 1(Editor)이 아닌 경우
-    if (!checkBoardMemberRole) {
-      throw new NotFoundException('권한이 없습니다.');
-    }
-
     // ERR : 삭제하려는 멤버의 role이 0(Admin)인 경우
-    if (checkBoardMemberRole.role === 0) {
+    if (boardMember.role === 0) {
       throw new NotFoundException('Admin은 삭제할 수 없습니다.');
     }
 
@@ -128,13 +95,47 @@ export class BoardMemberService {
     return { message: '보드 멤버 삭제가 완료되었습니다.' };
   }
 
-  // 보드 존재 여부 조회
-  async findBoard(board_id: number): Promise<Board | undefined> {
-    return this.boardRepository.findOne({ where: { id: board_id } });
+  // 보드 존재 여부 확인
+  async findOneBoard(board_id: number): Promise<Board | any> {
+    const existingBoard = await this.boardRepository.findOne({ where: { id: board_id } });
+    // ERR : 보드가 존재하지 않을 경우
+    if (!existingBoard) {
+      throw new NotFoundException('보드가 존재하지 않습니다.');
+    }
+    return existingBoard;
   }
 
   // 보드 멤버 권한 조회
   async findBoardMember(board_id: number, user_id: number) {
     return this.boardMemberRepository.findOne({ where: { board_id, user_id } });
+  }
+
+  // 보드 멤버 여부 조회
+  async checkBoardMember(board_id: number, user_id: number): Promise<BoardMember | any> {
+    const exsitingBoardMember = await this.boardMemberRepository.findOne({ where: { board_id, user_id } });
+
+    // ERR : 보드 멤버가 아닌 경우
+    if (!exsitingBoardMember) {
+      throw new NotFoundException('권한이 없습니다.');
+    }
+  }
+
+  // 로그인 한 사용자의 권한 확인
+  async checkBoardMemberRole(board_id: number, user_id: number): Promise<Board | any> {
+    // role이 0(Admin) 또는 1(Editor) 일 때 수정/삭제 가능
+    // join문으로 처리해야하남? - 이아영
+    const existingBoardMember = await this.boardMemberRepository.findOne({
+      where: [
+        { board_id, user_id, role: 0 },
+        { board_id, user_id, role: 1 },
+      ],
+    });
+
+    // ERR : role가 0(Admin), 1(Editor)이 아닌 경우
+    if (!existingBoardMember) {
+      throw new UnauthorizedException('권한이 존재하지 않습니다.');
+    }
+
+    return existingBoardMember;
   }
 }
