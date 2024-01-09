b288f5b9e23dcc4c11c705e1e83976f0c1a7f449
da <dain940209@gmail.com>
2024-01-09 22:25:30 +0900

    fix front : - 보드 삭제 버튼 추가 - 보드 이름 받아오기 기능 추가 - 토큰이 유효하지 않으면 로컬스토리지에서 토큰 삭제

--- public/sidebar.js
+++ public/sidebar.js
@@ -1,5 +1,5 @@
-import { loadBoardList } from "./board.js";
-import { showLoginModal, showSignupModal, showProfileModal } from "./user.js";
+import { loadBoardList } from './board.js';
+import { showLoginModal, showSignupModal, showProfileModal } from './user.js';
 
 // Sidebar 생성
 export function initailizeSideBar() {
@@ -7,57 +7,58 @@ export function initailizeSideBar() {
   showSidebarState(false);
 }
 
-export function showSidebarState() {
+export async function showSidebarState() {
   const accessToken = localStorage.getItem('access_token');
   if (accessToken !== null) {
-    showSideStatelogin();
-    loadBoardList();
+    // 회원 정보 조회 API
+    await axios
+      .get('/user/profile', {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((response) => {
+        const user = response.data.user;
+        showSideStatelogin(user.name);
+      })
+      .catch((error) => {
+        // console.log('error: ', error);
+        // alert(error.response.data.message);
+        localStorage.removeItem('access_token');
+        location.reload();
+      });
   } else {
     showSideStatelogout();
   }
 }
 
-
-
-
-async function showSideStatelogin() {
-  // 회원 정보 조회 API
-  const accessToken = await localStorage.getItem('access_token');
-  await axios.get('/user/profile', {
-    headers: { Authorization: `Bearer ${accessToken}` }
-  })
-    .then(response => {
-      const user = response.data.user;
-      const text = document.getElementById("sidebar-text");
-      text.innerText = `${user.name}님 안녕하세요.`;
-    })
-    .catch(error => {
-      console.log('error: ', error);
-      alert(error.response.data.message);
-    });
-  document.getElementById("login-button").style.display = "none";
-  document.getElementById("signup-button").style.display = "none";
-  document.getElementById("logout-button").style.display = "block";
-  document.getElementById("profile-button").style.display = "block";
+async function showSideStatelogin(username) {
+  const text = document.getElementById('sidebar-text');
+  text.innerText = `${username}님 안녕하세요.`;
+  document.getElementById('login-button').style.display = 'none';
+  document.getElementById('signup-button').style.display = 'none';
+  document.getElementById('logout-button').style.display = 'block';
+  document.getElementById('profile-button').style.display = 'block';
+  document.getElementById('board-list-text').style.display = 'block';
+  loadBoardList();
 }
 
 function showSideStatelogout() {
-  const text = document.getElementById("sidebar-text");
+  const text = document.getElementById('sidebar-text');
   text.innerText = `로그인이 필요합니다.`;
-  document.getElementById("login-button").style.display = "block";
-  document.getElementById("signup-button").style.display = "block";
-  document.getElementById("logout-button").style.display = "none";
-  document.getElementById("profile-button").style.display = "none";
+  document.getElementById('login-button').style.display = 'block';
+  document.getElementById('signup-button').style.display = 'block';
+  document.getElementById('logout-button').style.display = 'none';
+  document.getElementById('profile-button').style.display = 'none';
+  document.getElementById('board-list-text').style.display = 'none';
 }
 
 function createSidebar() {
-  const main = document.getElementById("main");
-  const button = document.createElement("button");
-  button.id = "menu-button";
-  button.style.backgroundColor = "#a2cfff";
-  button.style.border = "2px solid white";
-  button.style.color = "#white";
-  button.style.padding = "4px";
+  const main = document.getElementById('main');
+  const button = document.createElement('button');
+  button.id = 'menu-button';
+  button.style.backgroundColor = '#a2cfff';
+  button.style.border = '2px solid white';
+  button.style.color = '#white';
+  button.style.padding = '4px';
   button.innerHTML = `<span class="material-symbols-outlined">
   arrow_forward_ios
   </span>`;
@@ -65,61 +66,57 @@ function createSidebar() {
   button.onclick = openNav;
   main.appendChild(button);
 
-  const sidebar = document.getElementById("sidebar");
+  const sidebar = document.getElementById('sidebar');
 
   // Close 버튼 생성
-  const closeBtn = document.createElement("div");
-  closeBtn.href = "javascript:void(0)";
-  closeBtn.classList.add("close-btn");
-  closeBtn.textContent = "×";
+  const closeBtn = document.createElement('div');
+  closeBtn.href = 'javascript:void(0)';
+  closeBtn.classList.add('close-btn');
+  closeBtn.textContent = '×';
   closeBtn.onclick = closeNav;
   sidebar.appendChild(closeBtn);
 
-  const text = document.createElement("p");
-  text.id = "sidebar-text";
+  const text = document.createElement('p');
+  text.id = 'sidebar-text';
   text.innerHTML = `<span class="material-symbols-outlined">
   account_circle
   </span>`;
   sidebar.appendChild(text);
 
   // 로그인 해야 할 때
-  const notUserbox = document.createElement("div");
-  notUserbox.id = "sidebar-not-user";
-  notUserbox.classList.add("button-box");
+  const notUserbox = document.createElement('div');
+  notUserbox.id = 'sidebar-not-user';
+  notUserbox.classList.add('button-box');
   sidebar.appendChild(notUserbox);
 
   // 로그인 버튼 생성
-  const loginButton = document.createElement("button");
-  loginButton.id = "login-button";
-  loginButton.textContent = "로그인";
-  loginButton.addEventListener("click", showLoginModal);
+  const loginButton = document.createElement('button');
+  loginButton.id = 'login-button';
+  loginButton.textContent = '로그인';
+  loginButton.addEventListener('click', showLoginModal);
   notUserbox.appendChild(loginButton);
 
   // 회원가입 버튼 생성
-  const signupButton = document.createElement("button");
-  signupButton.id = "signup-button";
-  signupButton.textContent = "회원가입";
-  signupButton.addEventListener("click", showSignupModal);
+  const signupButton = document.createElement('button');
+  signupButton.id = 'signup-button';
+  signupButton.textContent = '회원가입';
+  signupButton.addEventListener('click', showSignupModal);
   notUserbox.appendChild(signupButton);
 
   // 로그인 하고 나서
-  const userbox = document.createElement("div");
-  userbox.id = "sidebar-not-user";
-  userbox.classList.add("button-box");
+  const userbox = document.createElement('div');
+  userbox.id = 'sidebar-not-user';
+  userbox.classList.add('button-box');
   sidebar.appendChild(userbox);
 
   // 로그아웃 버튼 생성
-  const logoutButton = document.createElement("button");
-  logoutButton.id = "logout-button";
-  logoutButton.textContent = "로그아웃";
-  logoutButton.addEventListener("click", () => {
+  const logoutButton = document.createElement('button');
+  logoutButton.id = 'logout-button';
+  logoutButton.textContent = '로그아웃';
+  logoutButton.addEventListener('click', () => {
     const accessToken = localStorage.getItem('access_token');
     axios
-      .post(
-        '/user/logout',
-        {},
-        { headers: { Authorization: `Bearer ${accessToken}` } },
-      )
+      .post('/user/logout', {}, { headers: { Authorization: `Bearer ${accessToken}` } })
       .then((response) => {
         localStorage.removeItem('access_token');
         location.reload();
@@ -131,34 +128,34 @@ function createSidebar() {
   userbox.appendChild(logoutButton);
 
   // 프로필보기 버튼 생성
-  const myButton = document.createElement("button");
-  myButton.id = "profile-button";
-  myButton.textContent = "프로필";
-  myButton.addEventListener("click", showProfileModal);
+  const myButton = document.createElement('button');
+  myButton.id = 'profile-button';
+  myButton.textContent = '프로필';
+  myButton.addEventListener('click', showProfileModal);
   userbox.appendChild(myButton);
 
-  const boardtext = document.createElement("p");
+  const boardtext = document.createElement('p');
   boardtext.innerText = `내 보드 목록`;
+  boardtext.id = 'board-list-text';
   sidebar.appendChild(boardtext);
 
-  const boardList = document.createElement("div");
-  boardList.id = "board-list";
-  boardList.classList.add("board-list");
+  const boardList = document.createElement('div');
+  boardList.id = 'board-list';
+  boardList.classList.add('board-list');
   sidebar.appendChild(boardList);
 }
 
-
 function openNav() {
-  document.getElementById("sidebar");
-  const sidebar = document.getElementById("sidebar");
-  sidebar.style.left = "0px";
-  document.getElementById("main-container").style.marginLeft = "250px";
-  document.getElementById("menu-button").style.display = "none";
+  document.getElementById('sidebar');
+  const sidebar = document.getElementById('sidebar');
+  sidebar.style.left = '0px';
+  document.getElementById('main-container').style.marginLeft = '250px';
+  document.getElementById('menu-button').style.display = 'none';
 }
 
 function closeNav() {
-  const sidebar = document.getElementById("sidebar");
-  sidebar.style.left = "-250px"; // sidebar의 폭만큼 왼쪽으로 이동
-  document.getElementById("main-container").style.marginLeft = "0px";
-  document.getElementById("menu-button").style.display = "block";
+  const sidebar = document.getElementById('sidebar');
+  sidebar.style.left = '-250px'; // sidebar의 폭만큼 왼쪽으로 이동
+  document.getElementById('main-container').style.marginLeft = '0px';
+  document.getElementById('menu-button').style.display = 'block';
 }
