cd4aeaed9e0daa669e676c86160950981f0f679f
jeongseon0 <86090167+jeongseon0@users.noreply.github.com>
2024-01-10 11:28:06 +0900

    [Fix] comment Entity 관계 설정 fix 및 service 수정

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -3,9 +3,11 @@ import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
 import { Comment } from './entities/comment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Card } from 'src/card/entities/card.entity';
+import { BoardMember } from 'src/board-member/entities/board-member.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [TypeOrmModule.forFeature([Comment, Card, BoardMember])],
   controllers: [CommentController],
   providers: [CommentService],
   exports: [CommentService],
